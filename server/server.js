ce9cbf0c22532124f1085abd4ef948302b294cf8
Tracy Yu <tracyyu93@g.ucla.edu>
2019-03-27 15:52:28 -0700

    Update changes made to webpack & package.json files

--- server/server.js
+++ server/server.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
+const bodyParser = require('body-parser');
+const db = require('../database/index.js');
 const app = express();
 const port = process.env.PORT || 3002;
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended : true }));
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, '../public')));
 
--- webpack.config.js
+++ webpack.config.js
@@ -2,16 +2,17 @@ const webpack = require('webpack');
 const path = require('path');
 
 module.exports = {
-  context: __dirname + '/client',
-  entry: './index.js',
+  entry: __dirname + '/client/index.jsx',
   module: {
-    loaders: [
+    rules: [
       {
         test: /\.jsx?$/,
         exclude: /node_modules/,
         loader: 'babel-loader',
-        query: {
-          presets: ['react', 'es2015', 'env']
+        options: {
+          presets: [
+            "@babel/env",
+            "@babel/react"]
         },
       },
     ],
